a0fa3fcfd990c7d56de74768fa97756fdabe2aa4
Edward20uprm <edward.carde@upr.edu>
2024-11-19 09:56:30 -0400

    Se añadió la clase ReservationSystem.  Constará de las clases Asientos, Clientes y estadio.

--- ReservationSystem.java
+++ ReservationSystem.java
@@ -1,3 +1,65 @@
+import java.util.ArrayList; 
+import java.util.LinkedList; 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+import java.util.Queue; 
 
 
-//Hola
+public class ReservationSystem{
+
+    class Asientos{
+        private int numero; 
+        private double precio; 
+        private boolean disponible; 
+        private String cliente; 
+        private String seccion; 
+        
+        public Asientos(int numero,String seccion, double precio){
+            this.numero = numero; 
+            this.precio = precio; 
+            this.disponible = true; 
+            this.cliente = null; 
+            this.seccion = seccion; 
+        } 
+
+        public boolean reservacion(String cliente){ 
+            if(disponible) {
+                this.disponible = false; 
+                this.cliente = cliente; 
+                return true; 
+            }
+            return false; 
+        }
+
+        public String cancelarReserva(){  //Devuelve el cliente que canceló la reserva
+            if(!disponible) {
+                this.disponible = true; 
+                String clienteActual = cliente; 
+                this.cliente = null; 
+                return clienteActual; 
+
+            }
+            return null; 
+        }
+
+        public boolean isDisponible() {
+            return disponible;
+        }
+
+        public int getNumero() {
+            return numero;
+        }
+
+        public String getSeccion() {
+            return seccion;
+        }
+
+        public double getPrecio() {
+            return precio;
+        }
+
+    }   
+    
+
+}
